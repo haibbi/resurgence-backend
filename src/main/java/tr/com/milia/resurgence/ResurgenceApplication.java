460a9e8c9dca875b06c6f6da87cef43c587ba8a7
Halil İbrahim <hibrahimbinol@gmail.com>
2020-04-13 19:29:58 +0300

    Initial commit

---
+++ src/main/java/tr/com/milia/resurgence/ResurgenceApplication.java
@@ -0,0 +1,13 @@
+package tr.com.milia.resurgence;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResurgenceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResurgenceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tr/com/milia/resurgence/ResurgenceApplicationTests.java
@@ -0,0 +1,13 @@
+package tr.com.milia.resurgence;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResurgenceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
