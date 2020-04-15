c0de0174e6bbe852220b7f954c7caf4441c56725
Halil İbrahim <hibrahimbinol@gmail.com>
2020-04-16 00:34:18 +0300

    added access token verifier

--- src/main/java/tr/com/milia/resurgence/security/TokenAuthenticationConverter.java
+++ src/main/java/tr/com/milia/resurgence/security/TokenAuthenticationConverter.java
@@ -1,5 +1,8 @@
 package tr.com.milia.resurgence.security;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.algorithms.Algorithm;
 import org.springframework.http.HttpHeaders;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.server.authentication.ServerAuthenticationConverter;
@@ -8,14 +11,25 @@ import reactor.core.publisher.Mono;
 
 public class TokenAuthenticationConverter implements ServerAuthenticationConverter {
 	private static final String BEARER_TOKEN_PREFIX = "Bearer ";
+	private final JWTVerifier verifier;
+
+	public TokenAuthenticationConverter() {
+		this.verifier = JWT.require(Algorithm.HMAC512("s3cr3t"))
+			.withIssuer("milia")
+			.withAudience("access")
+			.build();
+	}
 
 	@Override
 	public Mono<Authentication> convert(ServerWebExchange exchange) {
-		String authorizationToken = exchange.getRequest().getHeaders().getFirst(HttpHeaders.AUTHORIZATION);
-		if (authorizationToken == null || !authorizationToken.startsWith(BEARER_TOKEN_PREFIX)) {
+		String authorizationHeader = exchange.getRequest().getHeaders().getFirst(HttpHeaders.AUTHORIZATION);
+		if (authorizationHeader == null || !authorizationHeader.startsWith(BEARER_TOKEN_PREFIX)) {
 			return Mono.empty();
 		}
 
-		return Mono.just(new JWTAuthenticationToken(authorizationToken.substring(BEARER_TOKEN_PREFIX.length())));
+		var accessToken = authorizationHeader.substring(BEARER_TOKEN_PREFIX.length());
+		verifier.verify(accessToken);
+
+		return Mono.just(new JWTAuthenticationToken(accessToken));
 	}
 }
