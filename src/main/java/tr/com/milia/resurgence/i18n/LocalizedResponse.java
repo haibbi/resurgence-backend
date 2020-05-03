83eadf4b59bd847b29d1aa63925e79501c45f929
Halil İbrahim <hibrahimbinol@gmail.com>
2020-05-03 13:53:14 +0300

    added detail to LocalizedResponse

--- src/main/java/tr/com/milia/resurgence/i18n/LocalizedResponse.java
+++ src/main/java/tr/com/milia/resurgence/i18n/LocalizedResponse.java
@@ -1,13 +1,23 @@
 package tr.com.milia.resurgence.i18n;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 public class LocalizedResponse {
 	private final String message;
 
-	public LocalizedResponse(String message) {
+	@JsonIgnoreProperties({"stack_trace", "codes", "suppressed"})
+	private final LocalizedException detail;
+
+	public LocalizedResponse(String message, LocalizedException detail) {
 		this.message = message;
+		this.detail = detail;
 	}
 
 	public String getMessage() {
 		return message;
+	}
+
+	public LocalizedException getDetail() {
+		return detail;
 	}
 }
