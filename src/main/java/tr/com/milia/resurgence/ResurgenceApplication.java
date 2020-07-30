278160830056a9779efc777d12941e3b6e4ec9a3
Halil İbrahim <hibrahimbinol@gmail.com>
2020-07-30 21:55:57 +0300

    added logbook

--- src/main/java/tr/com/milia/resurgence/ResurgenceApplication.java
+++ src/main/java/tr/com/milia/resurgence/ResurgenceApplication.java
@@ -2,8 +2,6 @@ package tr.com.milia.resurgence;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.filter.CommonsRequestLoggingFilter;
 
 @SpringBootApplication
 public class ResurgenceApplication {
@@ -11,15 +9,4 @@ public class ResurgenceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ResurgenceApplication.class, args);
 	}
-
-	@Bean
-	CommonsRequestLoggingFilter requestLoggingFilter() {
-		CommonsRequestLoggingFilter logFilter = new CommonsRequestLoggingFilter();
-		logFilter.setIncludeClientInfo(true);
-		logFilter.setIncludeHeaders(true);
-		logFilter.setIncludePayload(true);
-		logFilter.setIncludeQueryString(true);
-		return logFilter;
-	}
-
 }
