e80dbbf26940a3972ec32911a3cdd86bb3fb7df1
Halil İbrahim <hibrahimbinol@gmail.com>
2020-08-21 20:59:42 +0300

    fix test

--- src/main/java/tr/com/milia/resurgence/firebase/FirebaseConfiguration.java
+++ src/main/java/tr/com/milia/resurgence/firebase/FirebaseConfiguration.java
@@ -3,6 +3,7 @@ package tr.com.milia.resurgence.firebase;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -18,8 +19,11 @@ public class FirebaseConfiguration {
 	private String databaseUrl;
 	private String storageBucket;
 	private String credentialFile;
+	private boolean enabled;
 
 	@Bean(destroyMethod = "delete")
+	@ConditionalOnProperty(prefix = "resurgence.firebase", name = "enabled", havingValue = "true",
+		matchIfMissing = true)
 	FirebaseApp firebaseApp() throws IOException {
 		var file = new File(credentialFile);
 		var inputStream = new FileInputStream(file);
@@ -55,5 +59,13 @@ public class FirebaseConfiguration {
 
 	public void setCredentialFile(String credentialFile) {
 		this.credentialFile = credentialFile;
+	}
+
+	public boolean isEnabled() {
+		return enabled;
+	}
+
+	public void setEnabled(boolean enabled) {
+		this.enabled = enabled;
 	}
 }
