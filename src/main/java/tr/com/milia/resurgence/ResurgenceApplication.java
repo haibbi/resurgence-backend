207f79cb5a3e1c1e7423161d5cd670ad0db1dc98
Halil İbrahim <hibrahimbinol@gmail.com>
2020-09-08 21:03:19 +0300

    logbook version 2.2.0

--- src/main/java/tr/com/milia/resurgence/ResurgenceApplication.java
+++ src/main/java/tr/com/milia/resurgence/ResurgenceApplication.java
@@ -2,9 +2,7 @@ package tr.com.milia.resurgence;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.web.filter.CommonsRequestLoggingFilter;
 
 @EnableAsync
 @SpringBootApplication
@@ -13,4 +11,5 @@ public class ResurgenceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ResurgenceApplication.class, args);
 	}
+
 }
