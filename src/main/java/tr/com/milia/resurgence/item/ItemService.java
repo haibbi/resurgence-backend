921bc9e5d3e328b5eea076ef67160d6021d0316c
Halil İbrahim <hibrahimbinol@gmail.com>
2020-09-19 15:22:50 +0300

    fix stock service price calculation

--- src/main/java/tr/com/milia/resurgence/item/ItemService.java
+++ src/main/java/tr/com/milia/resurgence/item/ItemService.java
@@ -1,11 +1,24 @@
 package tr.com.milia.resurgence.item;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Service;
 import tr.com.milia.resurgence.stock.StockEvent;
 
+import java.util.Arrays;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 @Service
 public class ItemService {
+
+	private static final Logger log = LoggerFactory.getLogger(ItemService.class);
+	private final Map<Item, Long> initialPrices;
+
+	public ItemService() {
+		initialPrices = Arrays.stream(Item.values()).collect(Collectors.toMap(i -> i, Item::getPrice));
+	}
 
 	void updateItemPrice(Item item, long price) {
 		item.setPrice(price);
@@ -14,7 +27,9 @@ public class ItemService {
 	@EventListener(StockEvent.class)
 	public void onStockEvent(StockEvent event) {
 		Item item = event.getItem();
-		long newPrice = item.getPrice() + event.getChange();
+		long change = event.getChange();
+		long newPrice = initialPrices.get(item) + change;
+		log.debug(item.name() + " price change. old/new/change {}/{}/{}", item.getPrice(), newPrice, change);
 		updateItemPrice(item, newPrice);
 	}
 
--- src/main/java/tr/com/milia/resurgence/stock/StockService.java
+++ src/main/java/tr/com/milia/resurgence/stock/StockService.java
@@ -1,25 +1,26 @@
 package tr.com.milia.resurgence.stock;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationEventPublisher;
 import tr.com.milia.resurgence.RandomUtils;
 import tr.com.milia.resurgence.item.Item;
 
 import java.time.Duration;
+import java.util.Arrays;
+import java.util.Map;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
 
 public class StockService {
 
-	private static final Logger log = LoggerFactory.getLogger(StockService.class);
-
 	private final ScheduledExecutorService executor = Executors.newSingleThreadScheduledExecutor();
 	private final ApplicationEventPublisher eventPublisher;
+	private final Map<Item, Long> initialPrices;
 
 	public StockService(ApplicationEventPublisher eventPublisher) {
 		this.eventPublisher = eventPublisher;
+		initialPrices = Arrays.stream(Item.values()).collect(Collectors.toMap(i -> i, Item::getPrice));
 	}
 
 	public void schedule(Duration duration, Item item, double change) {
@@ -31,12 +32,8 @@ public class StockService {
 		double ratio = RandomUtils.random(0, change);
 		if (RandomUtils.random() <= .50) ratio *= -1;
 
-		long amount = (long) (item.getPrice() * ratio);
-
-		long newPrice = item.getPrice() + amount;
-
-		log.debug(item.name() + " price has {}. old/new/ratio: {}/{}/{}",
-			ratio < 0 ? "down" : "up", item.getPrice(), newPrice, amount);
+		long initialPrice = initialPrices.get(item);
+		long amount = (long) (initialPrice * ratio);
 
 		eventPublisher.publishEvent(new StockEvent(item, amount));
 	}
